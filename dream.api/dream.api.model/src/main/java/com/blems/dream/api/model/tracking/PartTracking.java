8faedc0307824e8f66929252f78240e8922a14c3
sokhom <hay.sokhom@gmail.com>
2017-08-03 23:41:00 +0700

    tag

---
+++ dream.api/dream.api.model/src/main/java/com/blems/dream/api/model/tracking/PartTracking.java
@@ -0,0 +1,17 @@
+package com.blems.dream.api.model.tracking;
+
+import com.blems.dream.api.model.DefaultModel;
+
+public class PartTracking extends DefaultModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String abbr;
+	private int sortable;
+	private PartTrackingType type;
+	private String description;
+
+}
