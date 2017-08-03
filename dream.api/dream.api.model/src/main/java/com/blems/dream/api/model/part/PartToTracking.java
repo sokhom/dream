1ac6086f05fdb4d9af607e540ddf3c4d60dc0237
sokhom <hay.sokhom@gmail.com>
2017-08-03 23:58:06 +0700

    part tracking

---
+++ dream.api/dream.api.model/src/main/java/com/blems/dream/api/model/part/PartToTracking.java
@@ -0,0 +1,18 @@
+package com.blems.dream.api.model.part;
+
+import com.blems.dream.api.model.BasedModel;
+import com.blems.dream.api.model.tracking.PartTracking;
+
+public class PartToTracking extends BasedModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String nextValue;
+	private Part part;
+	private PartTracking partTracking;
+	private boolean primaryFlag;
+
+}
