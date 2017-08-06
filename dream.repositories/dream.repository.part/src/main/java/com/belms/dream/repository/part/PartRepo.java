c9d106d740b1a0623b1dd35fd8737866ad28b8e5
sokhom <hay.sokhom@gmail.com>
2017-08-06 20:11:12 +0700

    PartRepo

---
+++ dream.repositories/dream.repository.part/src/main/java/com/belms/dream/repository/part/PartRepo.java
@@ -0,0 +1,17 @@
+package com.belms.dream.repository.part;
+
+import java.util.List;
+
+import com.blems.dream.api.model.part.Part;
+
+import dream.repository.common.AbstractRepo;
+
+public class PartRepo extends AbstractRepo<Part> {
+	
+
+	public List getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
