871213b499b3c708f009bd6ccbdfbb1dcc88f1dd
sokhom <hay.sokhom@gmail.com>
2017-08-08 22:00:52 +0700

    v5

--- dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/PartDetail.java
+++ dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/PartDetail.java
@@ -1,7 +1,10 @@
 package com.belms.dream.workspace.part;
 
+import com.belms.dream.api.dto.part.PartInitDataWrapperDto;
 import com.belms.dream.api.view.EntryView;
 import com.blems.dream.api.model.part.Part;
+import com.vaadin.data.Binder;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.VerticalLayout;
 
@@ -11,6 +14,17 @@ public class PartDetail extends VerticalLayout  implements EntryView<Part>{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private final Binder<Part> binder;
+	
+	
+	public PartDetail(PartInitDataWrapperDto partInitDataWrapper) {
+		binder = new Binder<>();
+		addStyleName("profile-form");
+		setIcon(VaadinIcons.USER);
+		setCaption("Detail");
+		initUI();
+		
+	}
 
 	@Override
 	public boolean isValid() {
@@ -19,13 +33,16 @@ public class PartDetail extends VerticalLayout  implements EntryView<Part>{
 
 	@Override
 	public Component getView() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this;
 	}
 
 	@Override
 	public void loadData(Part data) {
-		// TODO Auto-generated method stub
+		binder.setBean(data);
+		
+	}
+	private void initUI(){
 		
 	}
 
