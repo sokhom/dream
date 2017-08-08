8d334b3489e546a1956173bde44ab0bd560103d9
sokhom <hay.sokhom@gmail.com>
2017-08-08 21:38:34 +0700

    PartDetail.java

---
+++ dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/PartDetail.java
@@ -0,0 +1,28 @@
+package com.belms.dream.workspace.part;
+
+import com.belms.dream.api.view.EntryView;
+import com.blems.dream.api.model.part.Part;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.VerticalLayout;
+
+public class PartDetail extends VerticalLayout  implements EntryView<Part>{
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Component getView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void loadData(Part data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
