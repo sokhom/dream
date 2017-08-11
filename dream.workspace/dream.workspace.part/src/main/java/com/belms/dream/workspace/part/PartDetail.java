1e90fdb56843e11ca86ffef0e1d6205746271371
hay.sokhom@gmail.com <hay.sokhom@gmail.com>
2017-08-11 18:05:38 +0700

    part detail--workspace

--- dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/PartDetail.java
+++ dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/PartDetail.java
@@ -1,5 +1,6 @@
 package com.belms.dream.workspace.part;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //import org.vaadin.grid.cellrenderers.editoraware.CheckboxRenderer;
@@ -8,18 +9,24 @@ import com.belms.dream.api.dto.part.PartInitDataWrapperDto;
 import com.belms.dream.api.view.EntryView;
 import com.blems.dream.api.model.part.Part;
 import com.blems.dream.api.model.part.PartToTracking;
+import com.blems.dream.api.model.product.Product;
 import com.vaadin.data.Binder;
 import com.vaadin.data.ValueProvider;
 import com.vaadin.data.provider.CallbackDataProvider;
 import com.vaadin.data.provider.DataProvider;
+import com.vaadin.event.LayoutEvents;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.SelectionMode;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.ValoTheme;
 
 public class PartDetail extends VerticalLayout  implements EntryView<Part>{
@@ -79,15 +86,12 @@ public class PartDetail extends VerticalLayout  implements EntryView<Part>{
 		
 		
 		formLayout.addComponent(trackingGrid);
+		formLayout.addComponent(getHLayout());
 		
 	}
 	
 	private Grid<PartToTracking> getTrackingGrid(){
-		Grid<PartToTracking> grid = new Grid<>("Tracking");
-		// Connect selected Customer to editor or hide if none is selected
-				grid.asSingleSelect().addValueChangeListener(e -> {
-//					editor.editCustomer(e.getValue());
-				});
+		Grid<PartToTracking> grid = new Grid<PartToTracking>("Tracking");	
 
 		// switch to multiselect mode
 		grid.setSelectionMode(SelectionMode.MULTI);
@@ -108,7 +112,53 @@ public class PartDetail extends VerticalLayout  implements EntryView<Part>{
 		
 //		grid.addColumn("Primary", new CheckboxRenderer());
 		
+		grid.asMultiSelect().addSelectionListener(event -> {	
+			event.getAllSelectedItems();
+			System.out.println("Hello");
+		});
+		
 		return grid;
+	}
+	private GridLayout getHLayout(){
+		GridLayout hLayout = new GridLayout(2,1);
+		hLayout.setCaption("Products");
+		Grid<Product> g = new Grid<Product>();
+		List<Product> itemList = new ArrayList<Product>();
+		g.setDataProvider(new CallbackDataProvider<>(query -> itemList.stream(), query -> itemList.size()));
+		g.addColumn(Product::getName).setCaption("Number");
+		g.addColumn(Product::getName).setCaption("Number");
+		g.addColumn(Product::getName).setCaption("Number");
+		g.setWidth(80, Unit.PERCENTAGE);
+		hLayout.addComponent(g);
+		VerticalLayout vLayout = getVButtonCRUDBar();
+		vLayout.setWidth(20, Unit.PERCENTAGE);
+		hLayout.addComponent(vLayout);
+		
+		return hLayout;
+	}
+	private VerticalLayout getVButtonCRUDBar(){
+		VerticalLayout vLayout = new VerticalLayout();
+		Button btnAdd = new Button(VaadinIcons.ADD_DOCK);
+		Button btnEdit = new Button(VaadinIcons.EDIT);
+		Button btnDelete = new Button(VaadinIcons.DEL);
+		btnAdd.addClickListener(event -> {
+			event.getButton();
+			System.out.println("Click on add button");			
+		});
+		btnEdit.addClickListener(event -> {
+			event.getButton();
+			System.out.println("Click on edit button");			
+		});
+		btnDelete.addClickListener(event -> {
+			event.getButton();
+			System.out.println("Click on delete button");			
+		});
+		
+		vLayout.addComponent(btnAdd);
+		vLayout.addComponent(btnEdit);
+		vLayout.addComponent(btnDelete);
+		
+		return vLayout;
 	}
 
 }
