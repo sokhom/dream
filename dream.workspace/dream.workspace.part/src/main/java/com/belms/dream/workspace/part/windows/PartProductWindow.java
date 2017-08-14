aa6832082a2f71ca51177d169cd55d9ccd6582b8
sokhom <hay.sokhom@gmail.com>
2017-08-14 20:24:33 +0700

    modified implement

--- dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/windows/PartProductWindow.java
+++ dream.workspace/dream.workspace.part/src/main/java/com/belms/dream/workspace/part/windows/PartProductWindow.java
@@ -1,5 +1,40 @@
 package com.belms.dream.workspace.part.windows;
 
-public class PartProductWindow {
+import com.belms.dream.api.view.event.SaveEntityListener;
+import com.belms.dream.workspace.common.window.AbstractSimpleDialog;
+import com.blems.dream.api.model.contact.Contact;
+import com.blems.dream.api.model.product.Product;
+import com.vaadin.ui.VerticalLayout;
+
+public class PartProductWindow extends AbstractSimpleDialog implements SaveEntityListener<Product>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void save(Product bean, com.belms.dream.api.view.event.SaveEntityListener.OPER_TYPE type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product getBean(com.belms.dream.api.view.event.SaveEntityListener.OPER_TYPE type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDialogCaption() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void buildContentLayout(VerticalLayout parent) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
